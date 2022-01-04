62bf840efdb05160f71b4aa069100e0b501616a4
Adam <adam.matheus.it@gmail.com>
2022-01-04 20:13:43 +0100

    ok

---
+++ build/precache-manifest.e9d429a8336fa7d0daa71bf17b36b8be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a2ed9aa06a2f3eee2b3b2e7c2aaf78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f2e1cb2214be25ef85c",
+    "url": "/static/css/main.30a23af7.chunk.css"
+  },
+  {
+    "revision": "e1c4e21724a78a9e8542",
+    "url": "/static/js/2.2c3ffba9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c3ffba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f2e1cb2214be25ef85c",
+    "url": "/static/js/main.f71594ab.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
